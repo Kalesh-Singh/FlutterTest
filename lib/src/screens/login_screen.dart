315d65858108a733713a836f4131ffbfcd224f6c
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-11-03 13:11:58 -0500

    Login Screen UI

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -7,9 +7,40 @@ class LoginScreen extends StatelessWidget {
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 20.0)),
+          submitButton(),
         ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'Enter your email address',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Enter your password',
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      onPressed: () {},
+      child: Text('Login'),
+      color: Colors.blue,
     );
   }
 }
