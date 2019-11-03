098297ac12530e3763a4cde15a5f3e1bf2a3d080
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-11-03 15:14:33 -0500

    Wiring up the validation mixins

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
+import '../mixins/validators.dart';
 
-class Bloc {
+class Bloc with Validators {
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
 
   // Add data to stream
-  Stream<String> get email => _emailController.stream;
-  Stream<String> get password => _passwordController.stream;
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+  Stream<String> get password => _passwordController.stream.transform(validatePassword);
 
   // Change data
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
+
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
 }
